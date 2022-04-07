fdacb67961149492c2fa08745f9e57afa707e398
MoeiSadeghi <37694875+iMoei@users.noreply.github.com>
2022-04-07 15:36:12 +0200

    Initial Commit

---
+++ HWS-001-SwiftUI/HWS_001_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_001_SwiftUIApp.swift
+//  HWS-001-SwiftUI
+//
+//  Created by iMommo on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct HWS_001_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
